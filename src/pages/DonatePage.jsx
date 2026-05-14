341e89fb00047b050f3b0da77d381b509b074b63
Khoa Phạm <doremonkhoa2005@gmail.com>
2026-05-14 15:06:39 +0700

    Update word

--- src/pages/DonatePage.jsx
+++ src/pages/DonatePage.jsx
@@ -152,7 +152,7 @@ export default function DonatePage() {
                       href="tel:+84915883688"
                       className="text-white no-underline font-['Times_New_Roman'] hover:opacity-80 transition"
                     >
-                      (+84) 91 588 36 88
+                      (+84) 91 588 36 88 - (Nguyễn Ngọc Kim Mai)
                     </a>
                   </p>
                 </div>
